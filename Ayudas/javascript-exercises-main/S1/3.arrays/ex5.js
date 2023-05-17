aa33152accdee1c84d30fd46c38597f50d518b59
Victor Cuellar <victorcuellar.parra@hotmail.com>
2023-05-17 12:21:51 +0200

    EjerciciosJS(Arrays)

---
+++ Ayudas/javascript-exercises-main/S1/3.arrays/ex5.js
@@ -0,0 +1,7 @@
+const RickAndMortyCharacters = ["Rick", "Beth", "Jerry", "Morty", "Summer", "Lapiz Lopez"];
+
+RickAndMortyCharacters.splice(5,1);
+
+console.log(RickAndMortyCharacters[0],RickAndMortyCharacters[4]);
+
+console.log(RickAndMortyCharacters[0], RickAndMortyCharacters[RickAndMortyCharacters.length -1]);
